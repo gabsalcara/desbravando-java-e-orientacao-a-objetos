773bc0c17e29edacd4bc6d5ab4bdc9a996239e18
Gabriel Alcara <gabriel.alcara@terra.com.br>
2024-01-04 18:50:14 -0300

    encapsulamento & getters and setters, modificadores de acesso

--- src/livraria/Livro.java
+++ src/livraria/Livro.java
@@ -1,12 +1,21 @@
 package livraria;
 
 public class Livro {
+
+	private String nome;
+	private String descricao;
+	private double valor;
+	private String isbn;
+	private Autor autor;
 	
-	String nome;
-	String descricao;
-	double valor;
-	String isbn;
-	Autor autor;
+	public Livro(Autor autor) {
+		this();
+		this.autor = autor;
+	}
+	
+	public Livro() {
+		this.isbn = "000-00-00000-00-0";
+	}
 	
 	void mostrarDetalhes() {
 		System.out.println("Mostrando detalhes do livro ");
@@ -22,12 +31,66 @@ public class Livro {
 		System.out.println("--");
 	}
 	
+	
 	public void aplicaDescontoDe(double porcentagem) {
+		if (porcentagem > 0.3) {
+			System.out.println("Desconto não pode ser maior do que 30%");
+		}
 		this.valor -= this.valor * porcentagem;
 	}
 	
 	boolean temAutor() {
 		return this.autor != null;
 	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+
+	public double getValor() {
+		return valor;
+	}
+
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+
+	public Autor getAutor() {
+		return autor;
+	}
+
+
+	public void setAutor(Autor autor) {
+		this.autor = autor;
+	}
+
 
 }
---
+++ src/livraria/RegrasDeDesconto.java
@@ -0,0 +1,18 @@
+package livraria;
+
+public class RegrasDeDesconto {
+	
+	public static void main(String[] args) {
+		
+//		Livro livro = new Livro();
+//		livro.adicionaValor(59.90);
+//		
+//		System.out.println("Valor atual: " + livro.retornaValor());
+//		
+//		if (!livro.aplicaDescontoDe(0.1)){
+//			System.out.println("Desconto não pode ser maior do que 30%");
+//		} else {
+//			System.out.println("Valor com desconto: ");
+//		}
+	}
+}
